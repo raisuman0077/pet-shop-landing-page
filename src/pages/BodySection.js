dd6ebc1e5439088094f715329feb8405f8574ec6
raisuman0077 <raisuman0077@gmail.com>
2022-09-03 15:24:42 +0545

    featured products items layout added

--- src/pages/BodySection.js
+++ src/pages/BodySection.js
@@ -1,9 +1,22 @@
+import React, {useState} from 'react';
 import flutterlogo from '../images/Flutterwave.png';
 import payoneerlogo from '../images/Payoneer.png';
 import paypallogo from '../images/PayPal.png';
 import petsmartlogo from '../images/PetSmart.png';
 import banner from '../images/Banner1.png'
+import product1 from '../products/product1.png'
+import product2 from '../products/product2.png'
+import product3 from '../products/product3.png'
+import product4 from '../products/product4.png'
+import product5 from '../products/product5.png'
+import product6 from '../products/product6.png'
+
+import { Rating } from 'react-simple-star-rating';
 const BodySection = () => {
+    const [rating, setRating] = useState(0);
+    const handleRating = (rate) => {
+        setRating(rate);
+    }
     return ( 
         <section className="section-body">
             <div className="section-b1 d-flex">
@@ -24,44 +37,101 @@ const BodySection = () => {
                     <img src={payoneerlogo} style={{width:'314px',height:"75px"}} alt="petsmart-logo" />
             </div>
             <h3 className='b2-text py-5'>Featured Products</h3>
-            <div className="row">
-                <div className="col">
-                    <div className="card h-100">
-                    <img src="..." className="card-img-top" alt="..." />
+            <div className="products mx-5">
+                <div className="product-detail">
+                    <div className="card mx-5">
+                    <img src={product1} className="card-img py-4 px-4" alt="product" />
                     <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <h5 className="card-title">Dog Rolling Stone</h5>
+                        <p className="card-text">N649.99</p>
+                        <Rating  className='rate-star'
+                            onClick={handleRating} 
+                            ratingValue={rating} 
+                            initialValue={4}
+                            allowHalfIcon 
+                            size={30} />
+                        </div>
+                    </div>
+                </div>
+                <div className="product-detail mx-5 my-2">
+                    <div className="card">
+                        <img src={product2} className="card-img py-4 px-4" alt="product" />                    
+                    <div className="card-body">
+                        <h5 className="card-title">Portable Water Bottle</h5>
+                        <p className="card-text">N499.89</p>
+                        <Rating className='rate-star'
+                            onClick={handleRating} 
+                            ratingValue={rating} 
+                            initialValue={4}
+                            allowHalfIcon 
+                            size={30} />
                     </div>
                     </div>
                 </div>
-                <div className="col">
-                    <div className="card h-100">
-                    <img src="..." className="card-img-top" alt="..." />
+                <div className="product-detail mx-5 my-2">
+                    <div className="card">
+                        <img src={product3} className="card-img py-4 px-4" alt="product" />
                     <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">This is a short card.</p>
+                        <h5 className="card-title">Dog Colling Mat</h5>
+                        <p className="card-text">N649.99</p>
+                        <Rating className='rate-star'
+                            onClick={handleRating} 
+                            ratingValue={rating} 
+                            initialValue={4}
+                            allowHalfIcon 
+                            size={30} />
                     </div>
                     </div>
                 </div>
-                <div className="col">
-                    <div className="card h-100">
-                    <img src="..." className="card-img-top" alt="..." />
+                <div className="product-details mx-5 my-2">
+                    <div className="card">
+                        <img src={product4} className="card-img py-4 px-4" alt="product" />
                     <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                    <h5 className="card-title">Portable Water Bottle</h5>
+                    <p className="card-text">N499.89</p>
+                        <Rating className='rate-star'
+                            onClick={handleRating} 
+                            ratingValue={rating} 
+                            initialValue={4}
+                            allowHalfIcon 
+                            size={30} />
                     </div>
                     </div>
                 </div>
-                <div className="col">
-                    <div className="card h-100">
-                    <img src="..." className="card-img-top" alt="..." />
+                <div className="product-detail mx-5 my-2">
+                    <div className="card">
+                        <img src={product5} className="card-img py-4 px-4" alt="product" />                    
                     <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <h5 className="card-title">Portable Water Bottle</h5>
+                        <p className="card-text">N499.89</p>
+                        <Rating className='rate-star'
+                            onClick={handleRating} 
+                            ratingValue={rating} 
+                            initialValue={4}
+                            allowHalfIcon 
+                            size={30} />
                     </div>
                     </div>
                 </div>
+                <div className="product-detail mx-5 my-2">
+                    <div className="card">
+                        <img src={product6} className="card-img py-4 px-4" alt="product" />
+                    <div className="card-body">
+                    <h5 className="card-title">Dog Colling Mat</h5>
+                    <p className="card-text">N649.99</p>
+                        <Rating className='rate-star'
+                            onClick={handleRating} 
+                            ratingValue={rating} 
+                            initialValue={4}
+                            allowHalfIcon 
+                            size={30} />
+                    </div>
+                    </div>
+                </div>         
             </div>
+            <div className='product-btn'>
+                <button className='btn morepdt-btn'>see more</button>
+                </div>
         </section>
      );
 }
